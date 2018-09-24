dce205708f921cfb9ca556c12e3b593fa57dc7dc
Peter Tillemans <pti@melexis.com>
2018-09-24 17:47:30 +0200

    finished chapter 3

--- src/main/scala/chapter3/Tree.scala
+++ src/main/scala/chapter3/Tree.scala
@@ -7,38 +7,26 @@ case class Node[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
-  // Exercise 3.25
-  def size[A](t: Tree[A]) : Int =
-    t match {
-      case Leaf(_) => 1
-      case Node(left, right) => length(left) + length(right)
-    }
-
-  // Exercise 3.26
-  def maximum(t: Tree[Int]) : Int =
-    t match {
-      case Leaf(x) => x
-      case Node(left, right) => maximum(left).max(maximum(right))
-    }
-
-  // Exercise 3.27
-  def depth[A](t: Tree[A]) : Int =
-    t match {
-      case Leaf(_) => 1
-      case Node(left, right) => 1 + depth(left).max(depth(right))
-    }
-
-  // Exercise 3.28
-  def map[A,B](t: Tree[A])(f: A => B) : Tree[B] =
-    t match {
-      case Leaf(x) => Leaf(f(x))
-      case Node(left, right) => Node(map(left)(f), map(right)(f))
-    }
-
   // Exercise 3.29
-  def fold[A, B](t: Tree[A])(f: (A,B) => B)(g: (B, B) => B) : B =
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B) : B =
     t match {
       case Leaf(x) => f(x)
       case Node(left, right) => g(fold(left)(f)(g), fold(right)(f)(g))
     }
+
+  def size[A](t: Tree[A]) : Int =
+    fold(t)(_ => 1)(_ + _)
+
+  // Exercise 3.26
+  def maximum(t: Tree[Int]) : Int =
+    fold(t)(x => x)(_.max(_))
+
+  // Exercise 3.27
+  def depth[A](t: Tree[A]) : Int =
+    fold(t)(_ => 1)(1 + _.max(_))
+
+  // Exercise 3.28
+  def map[A,B](t: Tree[A])(f: A => B) : Tree[B] =
+    fold(t)(a => Leaf(f(a)):Tree[B])(Node(_,_):Tree[B])
+
 }
